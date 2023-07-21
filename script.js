d593372312e81a589ed2fd47c236d1695982d69c
ManjuPaliwal <116282082+ManjuPaliwal@users.noreply.github.com>
2023-07-21 20:27:06 +0530

    commit

---
+++ script.js
@@ -0,0 +1,374 @@
+const menu = [
+    {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/945057664/photo/double-cheese-burger.jpg?s=1024x1024&w=is&k=20&c=Fxlr--dmHlxo0tHOxelbPdpfPPkp5UsvRn4jttvGJy0=',
+        title: 'Burger',
+        price: 'Price:- 250/-',
+        description: ' Juicy double cheeseburger. Made with two 100% beef patties, two melty slices of cheese, lettuce, tomatoes, onion, and pickles, on a fresh sesame seed bun.'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1252605687/photo/veg-momos-isolated-on-white-momos-is-the-popular-dish-of-indian-tibetan-chinese-cuisines.webp?s=1024x1024&w=is&k=20&c=n-R-LDIhb3ElZkqyIz5uLjsxPO_0Jwk6seUGz8i82GI=',
+        title: 'Momos',
+        price: 'Price:- 150/-',
+        description: 'Veg Momos isolated on white . Momos is the popular dish of indian, tibetan, chinese cuisines. Asian food. Vegetarian meal'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1284771650/photo/veg-manchurian-gravy-balls-in-black-bowl-isolated-on-white-vegetarian-manchurian-is-indian.webp?s=1024x1024&w=is&k=20&c=rr7ZjTHynOtlUU6BdHU1aBTZvQjcJD5AfSoHY6DJy-A=',
+        title: 'Manchurian Gravy',
+        price: 'Price:- 200/-',
+        description: 'Veg Manchurian Gravy Balls in black bowl isolated on white. Vegetarian Manchurian is indian chinese cuisine dish. Asian food and meal'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1429664332/photo/indian-veg-thali-food-platter-selective-focus-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=9XxWb0Mh3s8xKumvi8k8rUE42oNVOAGga8WatLvC4_o=',
+        title: 'Indian Veg Thal',
+        price: 'Price:- 150/-',
+        description: 'Indian Veg Thali / food platter, selective focus isolated '
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/458899491/photo/fries-and-nuggets.jpg?s=612x612&w=0&k=20&c=nHrNpAMUraFLOeMFDOIGRB4oDtKD-Yg_0WLvgESb4o4=',
+        title: 'Fries and Nuggets',
+        price: 'Price:- 80/- Per Each',
+        description: 'Fries and Nuggets in paper boxes '
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1306134869/photo/ghee-roast-dosa-and-idlii.jpg?s=612x612&w=0&k=20&c=Fc0C2u4hHnBVgvX__IyHaMszlpYk93yNtKGTMFbGyeo=',
+        title: 'Ghee roast Dosa and Idlii',
+        price: 'Price:- 40/- Per Each',
+        description: 'Ghee roast Dosa and Idlii, south indian main breakfast item which is beautifully arranged in a white plate lined with banana leaf and curry as sambar and chutney on white background.'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1413248571/photo/two-tacos-with-ground-beef-and-lime-on-white-background.jpg?s=612x612&w=0&k=20&c=QJC5RzLFTdavb_Zu56vkaspg4a00rYv9nqTxSPCtdq8=',
+        title: 'Tacos',
+        price: 'Price:- 50/-',
+        description: 'Two Tacos with ground beef and lime isolated'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1427819430/photo/pepperoni-pizza-on-white-with-ham-rucola-and-vegetables-in-isolated-on-a-white-background.webp?s=1024x1024&w=is&k=20&c=YS5sRPtl-aSyQq1Nadh2zFMsByd15J8UYPj21HbSbRc=',
+        title: 'Pepperoni Pizza',
+        price: 'Price:- 300/-',
+        description: 'Pepperoni pizza on white with ham, rucola, and vegetables in isolated'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1477093324/photo/square-pizza-table-top-view.jpg?s=1024x1024&w=is&k=20&c=Anwrorm-4XhKryGIwnYRlxFcf9HDYISnkggfuAhqas0=',
+        title: 'Square Pizza',
+        price: 'Price:- 200/-',
+        description: 'Fresh Square Pizza isolated'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1225951710/photo/portion-of-indian-samosas-on-black-plate-isolated.jpg?s=612x612&w=0&k=20&c=jrysch9ZzHzxNyZ_L7732xXKJRADnRl_vjBFad9T6I8=',
+        title: 'Samosas',
+        price: 'Price:- 70/-',
+        description: 'Samosas (fried savoury pastry)'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/1437059443/photo/crunchy-loaf-of-bread-with-sauce-table-top-spin.jpg?s=612x612&w=0&k=20&c=I7bcjmZbe2S4mkDBUd7bflMk7eW6k6IyZunT11CRrpM=',
+        title: 'Loaf Of Bread',
+        price: 'Price:- 80/-',
+        description: 'Crunchy homemade loaf of bread with sauce'
+    }, {
+        type: 'food',
+        image: 'https://media.istockphoto.com/id/155284812/photo/a-hotdog-with-chili-sauce-and-onions.webp?s=1024x1024&w=is&k=20&c=5adxBf4XkCrKzJg4eCJraGtwdJYVjGDbhkMA2OE-tFQ=',
+        title: 'Chili Dog ',
+        price: 'Price:- 120/-',
+        description: 'Chili cheese dog with onions '
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/524153833/photo/glass-of-cola-fanta-sprite-isolated-with-clipping-path.jpg?s=612x612&w=0&k=20&c=tG8ACEU0nLb1cvphC3wBTZ3VAi_3m8Jb8fpayUd0IIE=',
+        title: 'Cola, Fanta, Sprite',
+        price: 'Price:- 50/- Per Each',
+        description: 'Glass of cola, fanta, sprite with ice cubes isolated '
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/185285647/photo/lemonade.jpg?s=612x612&w=0&k=20&c=ZrOxo14Q-6-RI2Rzz8eWtbvr6x14upffYjPx7YRgnKY=',
+        title: 'Cold Lemonade',
+        price: 'Price:- 60/-',
+        description: 'Glass of cold lemonade served and garnished'
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/808733230/photo/drink-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=Zi-QtEXq5F5oZS-oQfjme3VRgDRwSsjLfwOtpgOjc0U=',
+        title: 'Watermelon',
+        price: 'Price:- 70/-',
+        description: 'Drink with watermelon and mint'
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/493742672/photo/hot-chocolate.jpg?s=612x612&w=0&k=20&c=dkKArBuc6ryyHZhmf-qXpKuDQt2GNwhIDvLmzeECbMg=',
+        title: 'Chocolate',
+        price: 'Price:- 120/-',
+        description: 'Hot Chocolate mug topped with whipped cream, chocolate and cinnamon'
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/1253019753/photo/a-glass-of-coconut-water-with-green-coconut-on-a-white-background.jpg?s=612x612&w=0&k=20&c=gWfnONgP1IOcU0UzRbh1k8kd3O-wYIi1gT7vjYrYJ8k=',
+        title: 'Coconut Water',
+        price: 'Price:- 50/-',
+        description: 'A Glass of coconut water with green coconut'
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/1282719164/photo/green-vegetable-smoothie-with-marijuana-in-a-jar.webp?s=1024x1024&w=is&k=20&c=VXmKbe6wvHBZ5d6IGcn_58ZicoLBN8qRxwGFw2Gpd5M=',
+        title: 'Vegetable Smoothie',
+        price: 'Price:- 130/-',
+        description: 'Green vegetable smoothie with marijuana leaves in a jar '
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/1413086158/photo/coffee-in-blue-cardboard-cup-on-white-background.jpg?s=612x612&w=0&k=20&c=idQKnvScw3wsr6d_DwcXGDdLnJTB4HaFvu2oMCbldC8=',
+        title: 'Coffee',
+        price: 'Price:- 80/-',
+        description: 'Coffee in blue cardboard cup'
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/1096407492/photo/hot-drink-herb-tea-or-assam-or-earl-grey-tea.jpg?s=612x612&w=0&k=20&c=1x5ZdFPvRsUtH2uzdt4H3LZg1GAb8__lQ06lWGRV8os=',
+        title: 'Cup of Tea',
+        price: 'Price:- 80/-',
+        description: 'Cup of tea with tea bag Hot drink, herb tea or assam or earl grey tea'
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/173501950/photo/fruit-smoothies.webp?s=1024x1024&w=is&k=20&c=Id2xAf1RteY5TpFEOiX5jX47RnnM8hNes3v6ttVlRGc=',
+        title: ' fruit Smoothie ',
+        price: 'Price:- 80/- Per Each',
+        description: 'Smoothie fruit juice milkshake with fruits'
+    }, {
+        type: 'drink',
+        image: 'https://media.istockphoto.com/id/518016169/photo/iced-coffee.jpg?s=612x612&w=0&k=20&c=cblmowamEy0Bf7kic5swVWyW9KRuPnHmj7F7Z2k9wN4=',
+        title: 'Iced caramel coffee',
+        price: 'Price:- 130/-',
+        description: 'Iced caramel coffee covered with whipped cream in plastic glass '
+    }
+]
+//scroll image
+const scrollImgDiv = document.querySelector('.scrollImgDiv');
+
+const nextbtn = document.createElement('button');
+nextbtn.classList.add('nextbtn');
+nextbtn.innerText = 'Next';
+
+scrollImgDiv.appendChild(nextbtn);
+const showScroll = () => {
+    let scrollImgArr = [];
+
+    while (scrollImgArr.length < 3) {
+        let randomIndex = Math.floor(Math.random() * menu.length);
+        if (!scrollImgArr.includes(randomIndex)) {
+            scrollImgArr.push(randomIndex);
+        }
+    }
+    scrollImgArr.forEach((index) => {
+        const img = document.createElement('img');
+        img.classList.add('scrollImg');
+        img.src = menu[index].image;
+
+        const title = document.createElement('p');
+        title.classList.add('title')
+        title.innerText = menu[index].title;
+
+
+        const div = document.createElement('div');
+        div.appendChild(img);
+        div.appendChild(title);
+        scrollImgDiv.appendChild(div);
+
+    })
+}
+showScroll();
+
+nextbtn.addEventListener('click', function () {
+    scrollImgDiv.innerHTML = '';
+    scrollImgDiv.appendChild(nextbtn);
+    scrollImgArr = [];
+    showScroll();
+});
+setInterval(()=>{
+    scrollImgDiv.innerHTML = '';
+    scrollImgDiv.appendChild(nextbtn);
+    showScroll();
+
+},2000)
+// show foods/drinks
+
+const values = document.querySelectorAll('.value');
+const closebtn = document.createElement('button');
+closebtn.innerHTML = '&times';
+closebtn.classList.add('closebtn')
+values.forEach((value) => {
+    value.addEventListener('click', () => {
+
+
+        const foodDrinkDiv = document.querySelector('.foodDrinkDiv');
+        foodDrinkDiv.innerHTML = '';
+        const foodtype = value.id;
+        console.log(foodtype);
+        menu.forEach((item, index) => {
+
+            // console.log(item.type);
+            if (item.type == foodtype) {
+
+                const img = document.createElement('img');
+                img.src = item.image;
+                img.classList.add('foodDrinkImg');
+
+                const title = document.createElement('p');
+                title.innerText = item.title;
+                title.classList.add('foodDrinkTitle');
+
+
+                const price = document.createElement('p');
+                price.innerText = item.price;
+                price.classList.add('foodDrinkPrice');
+
+
+                const description = document.createElement('p');
+                description.innerText = item.description;
+                description.classList.add('foodDrinkDescription');
+
+
+                const div = document.createElement('div');
+                div.classList.add('foodDrink');
+
+                const aboutfood = document.createElement('div');
+                aboutfood.classList.add('aboutfood')
+
+
+
+                foodDrinkDiv.appendChild(div);
+                div.appendChild(img);
+                div.appendChild(aboutfood)
+                aboutfood.appendChild(title);
+                aboutfood.appendChild(price);
+                aboutfood.appendChild(description);
+                div.appendChild(closebtn);
+
+
+                sectionheaderStyle();
+                closebtn.addEventListener('click', function () {
+                    foodDrinkDiv.innerHTML = '';
+                });
+            }
+
+        })
+        // console.log(food[0].type);
+    })
+})
+
+//food find(menu);
+
+const foodFind = document.querySelector('#foodFind');
+const foodfind = document.querySelector('.foodfind');
+foodfind.style.display = 'none';
+
+foodFind.addEventListener('click', function () {
+
+    foodfind.style.display = 'flex';
+
+    menu.forEach((item) => {
+        let divType;
+
+        const yummyMenu = () => {
+            const div = document.createElement('div');
+            div.classList.add('itemdiv');
+
+            const p = document.createElement('p');
+            p.classList.add('itemName');
+            p.innerText = item.title;
+
+            const price = document.createElement('p');
+            price.innerText = item.price;
+            price.classList.add('itemPrice')
+
+            divType.appendChild(div);
+            div.appendChild(p);
+            div.appendChild(price)
+            divType.appendChild(closebtn);
+
+        }
+
+        if (item.type == 'food') {
+            const foodMenu = document.querySelector('.foodMenu');
+            divType = foodMenu;
+            yummyMenu();
+        } else {
+            const drinkMenu = document.querySelector('.drinkMenu');
+            divType = drinkMenu;
+            yummyMenu();
+
+        }
+
+        sectionheaderStyle();
+        closebtn.addEventListener('click', function () {
+            const foodfind = document.querySelector('.foodfind');
+            const drinkMenu = document.querySelector('.drinkMenu');
+            const foodMenu = document.querySelector('.foodMenu');
+
+            foodMenu.innerHTML = '';
+            drinkMenu.innerHTML = '';
+            foodfind.style.display = 'none';
+        });
+
+    })
+})
+
+const searchBtn = document.querySelector('.search-btn');
+const input = document.querySelector('.input');
+const displayFood = document.querySelector('.displayFood');
+
+searchBtn.addEventListener('click', function () {
+    let inputValue = input.value.toLowerCase();
+    let found = false;
+
+    menu.forEach((item) => {
+        const items = item.title.toLowerCase();
+        if (items == inputValue) {
+            displayFood.style.display = 'flex'
+            const img = document.createElement('img');
+            img.src = item.image;
+            img.classList.add('displayImg');
+            const title = document.createElement('p');
+            title.innerText = item.title;
+            title.classList.add('displayTitle');
+
+
+            const price = document.createElement('p');
+            price.innerText = item.price;
+            price.classList.add('displayPrice');
+
+
+            const description = document.createElement('p');
+            description.innerText = item.description;
+            description.classList.add('dislayDescription');
+
+            displayFood.appendChild(img);
+            displayFood.appendChild(title);
+            displayFood.appendChild(price);
+            displayFood.appendChild(description);
+            displayFood.appendChild(closebtn);
+
+            sectionheaderStyle();
+            found = true
+        }
+    });
+      if (!found) {
+        displayFood.style.display = 'flex'
+        const p = document.createElement('p');
+        p.innerText = '"We apologize, but the requested food item is currently unavailable. Please take a moment to check our food find menu for alternative options that may pique your intrest..!"';
+        p.style.width='650px'
+        p.classList.add('displayTitle');
+        displayFood.appendChild(p);
+        displayFood.appendChild(closebtn);
+        sectionheaderStyle();
+
+    }
+})
+
+
+const sectionheaderStyle = () => {
+    const section = document.querySelector('.section');
+    section.classList.add('blur');
+
+    const header = document.querySelector('.header');
+    header.style.display = 'none';
+
+    closebtn.addEventListener('click', function () {
+        displayFood.innerHTML = '';
+        section.classList.remove('blur');
+        header.style.display = 'flex';
+
+    });
+}
+
